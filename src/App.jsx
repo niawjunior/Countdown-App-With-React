96f0208a6d15ef0f015468950f3c4337871e9f7d
niawkung <niawkung2@gmail.com>
2017-09-10 02:24:17 +0700

    create clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 class App extends Component {
     constructor(props) {
@@ -16,12 +17,7 @@ class App extends Component {
             <div className="App">
               <div className="App-title">Countdown to {this.state.deadline}
               </div>
-              <div>
-                <div className="Clock-days">14 Days</div>
-                <div className="Clock-hours"> 30 hours</div>
-                <div className="Clock-minutes">15 minutes</div>
-                <div className="Clock-seconds">20 seconds</div>
-              </div>
+              <Clock />
               <div>
                 <input 
                 placeholder='new date'
